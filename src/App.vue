c34f0e695abd0d0701138cc95ef91eac4c103a78
100race <46726709+100race@users.noreply.github.com>
2021-03-01 00:19:48 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 부트스트랩 Navbar UI 표시 -->
+    <!-- https://getbootstrap.com/docs/4.1/components/navbar/ -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <!-- 페이지 이동 설정 -->
+          <router-link class="nav-item nav-link" to="/">HOME</router-link>
+
+        </div>
+      </div>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+
+//vue 컴포넌트 임포트
 import App from './App.vue'
+import Home from '@/views/Home.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: Home
+    }
+  ]
+});
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
+
